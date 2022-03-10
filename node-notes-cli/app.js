96420887cf3beee0f12f3b67929fd20c6ed6cb49
Michael Moncera <michaelmoncera@gmail.com>
2022-03-10 20:49:02 +0000

    Created function to write data to data.json

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,41 +1,33 @@
 const fs = require('fs');
 const data = require('./data.json');
 
-if (process.argv[2] === 'read') {
-  let entries = Object.entries(data.notes);
-  entries = entries.map(entry => {
-    entry = `${entry[0]}: ${entry[1]}`;
-    return entry;
+const writeData = () => {
+  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+    if (err) {
+      throw err;
+    }
   });
-  console.log(entries.join('\n'));
+};
+
+if (process.argv[2] === 'read') {
+  const entries = Object.entries(data.notes);
+  entries.forEach(entry => console.log(`${entry[0]}: ${entry[1]}`));
 }
 
 if (process.argv.length === 4 && process.argv[2] === 'create') {
   data.notes[data.nextId] = process.argv[3];
   data.nextId++;
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-    if (err) {
-      throw err;
-    }
-  });
+  writeData();
 }
 
 if (process.argv[2] === 'delete') {
   delete data.notes[process.argv[3]];
-  fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-    if (err) {
-      throw err;
-    }
-  });
+  writeData();
 }
 
 if (process.argv.length === 5 && process.argv[2] === 'update') {
   if (process.argv[3] in data.notes) {
     data.notes[process.argv[3]] = process.argv[4];
-    fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
-      if (err) {
-        throw err;
-      }
-    });
+    writeData();
   }
 }
