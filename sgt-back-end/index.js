4d2a64a5b2306fb2a6ea2570a996fcc25f69fdc5
Michael Moncera <michaelmoncera@gmail.com>
2022-04-03 23:52:41 +0000

    Implemented GET request.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -4,13 +4,67 @@ const app = express();
 
 app.use(express.json());
 
-// only create ONE pool for your whole server
-// eslint-disable-next-line no-unused-vars
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+app.get('/api/grades', (req, res) => {
+  const sql = `
+    SELECT
+      *
+    FROM
+      "grades"
+  `;
+  db.query(sql)
+    .then(data => {
+      const grades = data.rows;
+      res.json(grades);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.get('/api/grades/:gradeId', (req, res) => {
+  const gradeId = +req.params.gradeId;
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: '"gradeId" must be a positive integer'
+    });
+    return;
+  }
+  const sql = `
+    SELECT
+      *
+    FROM
+      "grades"
+    WHERE
+      "gradeId" = $1
+  `;
+  const params = [gradeId];
+  db.query(sql, params)
+    .then(data => {
+      const grade = data.rows[0];
+      if (!grade) {
+        res.status(404).json({
+          error: `Cannot find grade with "gradeId" ${gradeId}`
+        });
+      } else {
+        res.json(grade);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
 });
 
 app.listen(3000, () => {
