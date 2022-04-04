79ca3b4c1ffc98531a916e569843e1650b8abfb7
Michael Moncera <michaelmoncera@gmail.com>
2022-04-04 23:59:08 +0000

    Implemented POST request.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -2,13 +2,24 @@ const express = require('express');
 const pg = require('pg');
 const app = express();
 
-app.use(express.json());
-
 const db = new pg.Pool({
   connectionString: 'postgres://dev:dev@localhost/studentGradeTable',
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+app.use(express.json());
+
+app.use('/api/grades/:gradeId', (req, res, next) => {
+  const gradeId = +req.params.gradeId;
+  if (!Number.isInteger(gradeId) || gradeId <= 0) {
+    res.status(400).json({
+      error: 'The "gradeId" must be a positive integer.'
+    });
+    return;
+  }
+  next();
 });
 
 app.get('/api/grades', (req, res) => {
@@ -33,12 +44,6 @@ app.get('/api/grades', (req, res) => {
 
 app.get('/api/grades/:gradeId', (req, res) => {
   const gradeId = +req.params.gradeId;
-  if (!Number.isInteger(gradeId) || gradeId <= 0) {
-    res.status(400).json({
-      error: '"gradeId" must be a positive integer'
-    });
-    return;
-  }
   const sql = `
     SELECT
       *
@@ -53,11 +58,45 @@ app.get('/api/grades/:gradeId', (req, res) => {
       const grade = data.rows[0];
       if (!grade) {
         res.status(404).json({
-          error: `Cannot find grade with "gradeId" ${gradeId}`
+          error: `Can not find grade with "gradeId" ${gradeId}.`
         });
       } else {
         res.json(grade);
       }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.post('/api/grades', (req, res) => {
+  const { name, course, score } = req.body;
+  if (!name || !course || !score) {
+    res.status(400).json({
+      error: 'The "name", "course", and "score" fields are required.'
+    });
+    return;
+  }
+  if (!Number.isInteger(+score) || +score < 0 || +score > 100) {
+    res.status(400).json({
+      error: 'The "score" field must be an integer from 0 to 100.'
+    });
+    return;
+  }
+  const sql = `
+    INSERT INTO
+      "grades" ("name", "course", "score")
+    VALUES
+      ($1, $2, $3) returning *
+  `;
+  const params = [name, course, +score];
+  db.query(sql, params)
+    .then(data => {
+      const grade = data.rows[0];
+      res.status(201).json(grade);
     })
     .catch(err => {
       console.error(err);
