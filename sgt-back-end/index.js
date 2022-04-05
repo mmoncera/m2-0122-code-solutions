c9445cbf7da2e3dde842bbbb03ca98f67d86c4ff
Michael Moncera <michaelmoncera@gmail.com>
2022-04-05 00:52:04 +0000

    Implemented PUT request.

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -92,11 +92,56 @@ app.post('/api/grades', (req, res) => {
     VALUES
       ($1, $2, $3) returning *
   `;
-  const params = [name, course, +score];
+  const params = [name, course, score];
   db.query(sql, params)
     .then(data => {
       const grade = data.rows[0];
       res.status(201).json(grade);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
+
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = +req.params.gradeId;
+  const { name, course, score } = req.body;
+  if (!name || !course || !score) {
+    res.status(400).json({
+      error: 'The "name", "course", and "score" fields are required.'
+    });
+    return;
+  }
+  if (!Number.isInteger(+score) || +score < 0 || +score > 100) {
+    res.status(400).json({
+      error: 'The "score" field must be an integer from 0 to 100.'
+    });
+    return;
+  }
+  const sql = `
+    UPDATE
+      "grades"
+    SET
+      "name" = $1,
+      "course" = $2,
+      "score" = $3
+    WHERE
+      "gradeId" = $4 returning *
+  `;
+  const params = [name, course, score, gradeId];
+  db.query(sql, params)
+    .then(data => {
+      const grade = data.rows[0];
+      if (!grade) {
+        res.status(404).json({
+          error: `Can not find grade with "gradeId" ${gradeId}.`
+        });
+      } else {
+        res.json(grade);
+      }
     })
     .catch(err => {
       console.error(err);
